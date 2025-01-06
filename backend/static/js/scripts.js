1801839c0e50de4a09797c973be1843c638819a9
arpit-bst <arpit.verma@bluestacks.com>
2025-01-06 17:26:21 +0530

    Delete backend directory

--- backend/static/js/scripts.js
+++
@@ -1,4 +0,0 @@
-// Example: Add an alert when refreshing the app list
-document.getElementById("refresh-button").addEventListener("click", function () {
-    alert("Re-scraping apps. Please wait...");
-});
